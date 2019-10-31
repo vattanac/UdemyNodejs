565392f7057b21c329a645898348080ae0594751
vattanac <vattanacsim99@gmail.com>
2019-10-31 11:27:21 +0700

    use express router adding 404 Error Page

--- myapp.js
+++ myapp.js
@@ -1,12 +1,18 @@
 const http = require('http');
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-const server = http.createServer(app);
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+});
+app.listen(3000);
 
 
