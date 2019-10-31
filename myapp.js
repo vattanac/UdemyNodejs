53f68cf3c154318759e052d475275213d7f237b0
vattanac <vattanacsim99@gmail.com>
2019-11-01 06:49:43 +0700

    returning page not found

--- myapp.js
+++ myapp.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,8 +11,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
+//MARK: Page Not Found
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
+    //res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendfile(path.join(__dirname, 'views', '404.html'));
 });
 app.listen(3000);
 
